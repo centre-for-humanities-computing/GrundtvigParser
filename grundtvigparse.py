7ba5568c8f6ecf373a87054c570b7767abf97784
Oliver Jarvis <oliversimonjarvis@gmail.com>
2019-02-26 22:46:46 +0100

    Add tag_finder

--- grundtvigparse.py
+++ grundtvigparse.py
@@ -19,25 +19,79 @@
 from lxml import etree, objectify
 import lxml.html, lxml.html.clean
 from argparse import ArgumentParser
-from itertools import chain
+#from GrundtvigTagValidator import metadata, body
+
+class GrundtvigParse:
+    #""" The main parser for grundtvig data"""
+    documentTree = None
+    def __init__(self, filename):
+
+        parse(filename)
+
+    #def getVersionNumber
+    def parse(self, filename, remove_comments=True):
+    #"""parse(filename, parser, remove_comments=True) filename = file to open for parsing parser = XMLParser() remove_comments = bool keep xml comments"""
+        parser = etree.XMLParser(remove_comments=remove_comments)
+        documentTree = objectify.parse("test_data.xml", parser=parser).getroot()
+
+    def metadata():
+    #""" def matadata()
+    #    get metadata in dictionary format
+    #"""
+        if(documentTree):
+            rootTag = getHead()
+            metadataDict = extractText(rootTag, validateTag.metadata, raw_text=False)
 
 
-def extract_text(from_element):
-    text = ""
-    for c in list(from_element):
-        if c.text:
-            text += c.text
-        if list(c):
-            text += extract_text(c)
-        if c.tail:
-            text += c.tail
-    return text
+    def extract_text(from_element):
+        text = ""
+        for c in list(from_element):
+            if c.text:
+                text += c.text
+            if list(c):
+                text += extract_text(c)
+            if c.tail:
+                text += c.tail
+        return text
+
+
+def is_tag(elem, tag_name):
+    return etree.QName(elem.tag).localname == tag_name
+
+def find_tag(root, tag_name, level):
+    found = False
+    if not is_tag(root, tag_name):
+        for c in list(root):
+            print(c.tag, level)
+            found = find_tag2(c, tag_name, level + 1)
+            if found:
+                return True
+    else:
+        return True
+    return False
+
+def get_tag(root, tag_name=None, attribute=None, attributeValue=None, recursive=False):
+        found = False
+        if not is_tag(root, tag_name):
+            for c in list(root):
+                print(c.tag, level)
+                found = find_tag2(c, tag_name, level + 1)
+                if recursive:
+                    return True
+        else:
+            return True
+        return False
 
 
 parser = etree.XMLParser(remove_comments=True)
-doc = objectify.parse("test_data.xml", parser=parser)
+documentTree = objectify.parse("test_data3.xml", parser=parser)
+root = documentTree.getroot()
+element = [x for x in root.iter("{http://www.tei-c.org/ns/1.0}teiHeader")]
 
-root = doc.getroot()
-element = [x for x in root.iter("{http://www.tei-c.org/ns/1.0}p")]
+if find_tag2(element[0], "idno", 0):
+    print("NEW")
+else:
+    print("OLD")
 
-print(extract_text(element[21]))
+
+#print(extract_text(element[19]))
